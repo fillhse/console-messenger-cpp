9dd03002c6497e7d8685a0c49cf3c6b152a88104
fillhseuni <fillhseuni@gmail.com>
2025-05-29 10:18:06 +0300

    Add /connect <ID>

--- client/main_client.cpp
+++ client/main_client.cpp
@@ -5,6 +5,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <sys/select.h>
 
 constexpr int PORT = 9090;
 constexpr int BUFFER_SIZE = 1024;
@@ -12,48 +13,42 @@ constexpr int BUFFER_SIZE = 1024;
 int main() {
     int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd == -1) {
-        perror("Ошибка при создании сокета");
+        perror("Ошибка создания сокета");
         return 1;
     }
 
-    sockaddr_in server_addr{};
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    sockaddr_in serv_addr{};
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-    if (connect(sock_fd, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Ошибка подключения к серверу");
+    if (connect(sock_fd, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Не удалось подключиться к серверу");
         return 1;
     }
 
+    fd_set read_fds;
     char buffer[BUFFER_SIZE]{};
-    fd_set readfds;
 
     while (true) {
-        FD_ZERO(&readfds);
-        FD_SET(sock_fd, &readfds);
-        FD_SET(STDIN_FILENO, &readfds);
+        FD_ZERO(&read_fds);
+        FD_SET(sock_fd, &read_fds);
+        FD_SET(STDIN_FILENO, &read_fds);
 
         int max_fd = std::max(sock_fd, STDIN_FILENO) + 1;
-        if (select(max_fd, &readfds, nullptr, nullptr, nullptr) < 0) {
-            perror("Ошибка select()");
-            break;
-        }
+        if (select(max_fd, &read_fds, nullptr, nullptr, nullptr) < 0) break;
 
-        if (FD_ISSET(sock_fd, &readfds)) {
-            ssize_t bytes = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
-            if (bytes <= 0) {
-                std::cout << "Сервер отключился\n";
-                break;
-            }
-            buffer[bytes] = '\0';
+        if (FD_ISSET(sock_fd, &read_fds)) {
+            ssize_t len = recv(sock_fd, buffer, sizeof(buffer) - 1, 0);
+            if (len <= 0) break;
+            buffer[len] = '\0';
             std::cout << buffer << std::flush;
         }
 
-        if (FD_ISSET(STDIN_FILENO, &readfds)) {
+        if (FD_ISSET(STDIN_FILENO, &read_fds)) {
             std::string input;
             std::getline(std::cin, input);
-            input += '\n';
+            input += "\n";
             send(sock_fd, input.c_str(), input.size(), 0);
         }
     }
--- server/main_server.cpp
+++ server/main_server.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <algorithm> // ← сюда подключается std::remove
 #include <string>
 #include <cstring>
+#include <unordered_map>
+#include <set>
+#include <vector>
+#include <sstream>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -8,16 +13,9 @@
 constexpr int PORT = 9090;
 constexpr int BUFFER_SIZE = 1024;
 
-int accept_client(int server_fd) {
-    sockaddr_in client_addr{};
-    socklen_t client_len = sizeof(client_addr);
-    int client_socket = accept(server_fd, (sockaddr*)&client_addr, &client_len);
-    if (client_socket < 0) {
-        perror("Ошибка при принятии подключения");
-        exit(EXIT_FAILURE);
-    }
-    return client_socket;
-}
+std::unordered_map<std::string, int> id_to_socket;
+std::unordered_map<int, std::string> socket_to_id;
+std::set<int> busy;
 
 std::string recv_message(int socket_fd) {
     char buffer[BUFFER_SIZE]{};
@@ -26,14 +24,40 @@ std::string recv_message(int socket_fd) {
     return std::string(buffer, bytes_read);
 }
 
-void send_message(int socket_fd, const std::string& message) {
-    send(socket_fd, message.c_str(), message.size(), 0);
+void send_message(int socket_fd, const std::string& msg) {
+    send(socket_fd, msg.c_str(), msg.size(), 0);
+}
+
+void handle_chat(int sock1, int sock2, const std::string& id1, const std::string& id2) {
+    send_message(sock1, "Вы начинаете чат с " + id2 + ". Для передачи хода введите /over\n");
+    send_message(sock2, "Ожидайте хода от " + id1 + ".\n");
+
+    int sender = sock1;
+    int receiver = sock2;
+    while (true) {
+        std::string msg = recv_message(sender);
+        if (msg.empty()) break;
+
+        if (msg == "/over\n" || msg == "/over") {
+            send_message(sender, "Вы передали ход.\n");
+            send_message(receiver, "Теперь ваша очередь. Введите /over, чтобы передать ход обратно.\n");
+            std::swap(sender, receiver);
+        } else {
+            std::string full = "[MSG] " + msg;
+            send_message(receiver, full);
+        }
+    }
+
+    send_message(sock1, "Чат завершён.\n");
+    send_message(sock2, "Чат завершён.\n");
+    busy.erase(sock1);
+    busy.erase(sock2);
 }
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
-        perror("Ошибка при создании сокета");
+        perror("Сокет не создан");
         return 1;
     }
 
@@ -42,53 +66,88 @@ int main() {
     server_addr.sin_port = htons(PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Ошибка при привязке сокета");
-        return 1;
-    }
+    bind(server_fd, (sockaddr*)&server_addr, sizeof(server_addr));
+    listen(server_fd, 10);
+    std::cout << "[СЕРВЕР] Готов к подключениям на порту " << PORT << "\n";
 
-    if (listen(server_fd, 2) < 0) {
-        perror("Ошибка при прослушивании");
-        return 1;
-    }
+    fd_set read_fds;
+    int max_fd = server_fd;
 
-    std::cout << "[СЕРВЕР] Ожидание подключений...\n";
-
-    int client1 = accept_client(server_fd);
-    send_message(client1, "Введите свой Telegram ID:\n");
-    std::string id1 = recv_message(client1);
-    std::cout << "[СЕРВЕР] Подключён клиент 1: " << id1 << "\n";
-
-    int client2 = accept_client(server_fd);
-    send_message(client2, "Введите свой Telegram ID:\n");
-    std::string id2 = recv_message(client2);
-    std::cout << "[СЕРВЕР] Подключён клиент 2: " << id2 << "\n";
-
-    send_message(client1, "Вы начинаете общение. Пишите сообщение. Для передачи хода введите /over\n");
-    send_message(client2, "Ожидайте, пока собеседник закончит.\n");
-
-    int sender = client1;
-    int receiver = client2;
+    std::vector<int> sockets;
 
     while (true) {
-        std::string msg = recv_message(sender);
-        if (msg.empty()) {
-            std::cout << "[СЕРВЕР] Один из клиентов отключился.\n";
-            break;
+        FD_ZERO(&read_fds);
+        FD_SET(server_fd, &read_fds);
+        for (int sock : sockets) FD_SET(sock, &read_fds);
+
+        select(max_fd + 1, &read_fds, nullptr, nullptr, nullptr);
+
+        if (FD_ISSET(server_fd, &read_fds)) {
+            int client_fd = accept(server_fd, nullptr, nullptr);
+            if (client_fd > 0) {
+                send_message(client_fd, "Введите ваш Telegram ID:\n");
+                std::string id = recv_message(client_fd);
+                id.erase(id.find_last_not_of("\r\n") + 1);
+
+                id_to_socket[id] = client_fd;
+                socket_to_id[client_fd] = id;
+                sockets.push_back(client_fd);
+                std::cout << "[+] Подключён ID: " << id << "\n";
+                send_message(client_fd, "Добро пожаловать. Используйте /connect <ID>, чтобы начать чат.\n");
+                if (client_fd > max_fd) max_fd = client_fd;
+            }
         }
 
-        if (msg == "/over\n" || msg == "/over") {
-            send_message(sender, "Вы передали ход.\n");
-            send_message(receiver, "Теперь ваша очередь. Для передачи хода введите /over\n");
-            std::swap(sender, receiver);
-        } else {
-            std::string full_msg = "[СООБЩЕНИЕ] " + msg;
-            send_message(receiver, full_msg);
+        for (int sock : sockets) {
+            if (FD_ISSET(sock, &read_fds)) {
+                std::string msg = recv_message(sock);
+                if (msg.empty()) {
+                    std::cout << "[-] Отключился: " << socket_to_id[sock] << "\n";
+                    close(sock);
+                    sockets.erase(std::remove(sockets.begin(), sockets.end(), sock), sockets.end());
+                    id_to_socket.erase(socket_to_id[sock]);
+                    socket_to_id.erase(sock);
+                    busy.erase(sock);
+                    continue;
+                }
+
+                msg.erase(msg.find_last_not_of("\r\n") + 1);
+
+                if (msg.rfind("/connect ", 0) == 0) {
+                    std::string target_id = msg.substr(9);
+                    if (id_to_socket.count(target_id) == 0) {
+                        send_message(sock, "Пользователь не найден.\n");
+                        continue;
+                    }
+                    int target_fd = id_to_socket[target_id];
+                    if (busy.count(target_fd)) {
+                        send_message(sock, "Пользователь занят.\n");
+                        continue;
+                    }
+                    std::string from_id = socket_to_id[sock];
+                    send_message(target_fd, "ID " + from_id + " хочет с вами связаться. Принять? (yes/no)\n");
+                    send_message(sock, "Ожидаем ответ собеседника...\n");
+
+                    std::string response = recv_message(target_fd);
+                    response.erase(response.find_last_not_of("\r\n") + 1);
+
+                    if (response == "yes") {
+                        send_message(sock, "Собеседник согласился. Начинаем чат...\n");
+                        send_message(target_fd, "Чат подтверждён. Начинаем...\n");
+                        busy.insert(sock);
+                        busy.insert(target_fd);
+                        handle_chat(sock, target_fd, from_id, target_id);
+                    } else {
+                        send_message(sock, "Собеседник отказался от чата.\n");
+                        send_message(target_fd, "Вы отказались от чата.\n");
+                    }
+                } else {
+                    send_message(sock, "Ожидание команды. Для начала чата: /connect <ID>\n");
+                }
+            }
         }
     }
 
-    close(client1);
-    close(client2);
     close(server_fd);
     return 0;
 }
