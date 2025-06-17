a2ccf6ca57ab2748348ed27d032a6f436a16eb9f
fillhseuni <fillhseuni@gmail.com>
2025-06-17 11:32:38 +0300

    Delete files from build

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Aholt/Desktop/console-messenger-cpp
-# Build directory: /mnt/c/Users/Aholt/Desktop/console-messenger-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/mnt/c/Users/Aholt/Desktop/console-messenger-cpp/build/run_tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/Aholt/Desktop/console-messenger-cpp/CMakeLists.txt;46;add_test;/mnt/c/Users/Aholt/Desktop/console-messenger-cpp/CMakeLists.txt;0;")
-subdirs("_deps/doctest-build")
